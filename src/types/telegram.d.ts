4c5597281a9ede866a08dd1e10fc746dcf94ec6a
Nathnael-G <nathnaelgetachew12f@gmail.com>
2026-07-24 10:45:27 +0300

    Testing onbaording 2 with modified tsconfig

---
+++ src/types/telegram.d.ts
@@ -0,0 +1,92 @@
+// src/types/telegram.d.ts
+export {};
+
+declare global {
+  interface Window {
+    Telegram?: {
+      WebApp: {
+        initData: string;
+        initDataUnsafe: {
+          user?: {
+            id: number;
+            first_name: string;
+            last_name?: string;
+            username?: string;
+            language_code?: string;
+            is_premium?: boolean;
+            allows_write_to_pm?: boolean;
+          };
+          chat?: {
+            id: number;
+            type: string;
+            title?: string;
+            username?: string;
+          };
+          chat_type?: string;
+          chat_instance?: string;
+          start_param?: string;
+          can_send_after?: number;
+        };
+        version: string;
+        platform: string;
+        themeParams: {
+          bg_color?: string;
+          text_color?: string;
+          hint_color?: string;
+          link_color?: string;
+          button_color?: string;
+          button_text_color?: string;
+          secondary_bg_color?: string;
+          header_bg_color?: string;
+          bottom_bar_bg_color?: string;
+          accent_text_color?: string;
+          section_bg_color?: string;
+          section_header_text_color?: string;
+          subtitle_text_color?: string;
+          destructive_text_color?: string;
+        };
+        isExpanded: boolean;
+        viewportHeight: number;
+        viewportStableHeight: number;
+        expand: () => void;
+        ready: () => void;
+        close: () => void;
+        sendData: (data: string) => void;
+        onEvent: (event: string, callback: () => void) => void;
+        offEvent: (event: string, callback: () => void) => void;
+        MainButton: {
+          text: string;
+          color: string;
+          textColor: string;
+          isVisible: boolean;
+          isActive: boolean;
+          setText: (text: string) => void;
+          setColor: (color: string) => void;
+          setTextColor: (color: string) => void;
+          show: () => void;
+          hide: () => void;
+          enable: () => void;
+          disable: () => void;
+          onClick: (callback: () => void) => void;
+          offClick: (callback: () => void) => void;
+        };
+        BackButton: {
+          isVisible: boolean;
+          show: () => void;
+          hide: () => void;
+          onClick: (callback: () => void) => void;
+          offClick: (callback: () => void) => void;
+        };
+        HapticFeedback: {
+          impactOccurred: (style: 'light' | 'medium' | 'heavy' | 'rigid' | 'soft') => void;
+          notificationOccurred: (type: 'error' | 'success' | 'warning') => void;
+          selectionChanged: () => void;
+        };
+        setHeaderColor: (color: string) => void;
+        setBackgroundColor: (color: string) => void;
+        enableClosingConfirmation: () => void;
+        disableClosingConfirmation: () => void;
+      };
+    };
+  }
+}
