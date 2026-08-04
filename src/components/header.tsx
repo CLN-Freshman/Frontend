560d0dd11fc0b0b146e9035b9153886db6f99cdf
Nathnael-G <nathnaelgetachew12f@gmail.com>
2026-08-04 20:41:42 +0300

    Added: Basic Notification page2

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -106,7 +106,7 @@ const Header: React.FC<HeaderProps> = ({ firstName, lastName, avatarUrl }) => {
           schema: 'public',
           table: 'announcements'
         },
-        (payload) => {
+        () => {
           fetchNotifications();
         }
       )
